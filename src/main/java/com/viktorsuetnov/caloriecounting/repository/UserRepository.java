5f59a6b8b5fc61fc9c915070b7892d7e2b034fa2
Viktor <suetnovvic@yandex.com>
2022-05-10 15:20:36 +0300

    добавил аннотацию Repository над интерфейсом

--- src/main/java/com/viktorsuetnov/caloriecounting/repository/UserRepository.java
+++ src/main/java/com/viktorsuetnov/caloriecounting/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.viktorsuetnov.caloriecounting.repository;
 
 import com.viktorsuetnov.caloriecounting.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> getUserById(Long id);
