e7a5d4b92c0d65aedbdc482e2435b9e693112ddf
Viktor <suetnovvic@yandex.com>
2022-05-11 19:26:08 +0300

    реализован метод doFilterInternal()

--- src/main/java/com/viktorsuetnov/caloriecounting/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/viktorsuetnov/caloriecounting/security/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,15 @@
 package com.viktorsuetnov.caloriecounting.security.jwt;
 
+import com.viktorsuetnov.caloriecounting.model.User;
+import com.viktorsuetnov.caloriecounting.security.SecurityConstants;
+import com.viktorsuetnov.caloriecounting.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -7,11 +17,43 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
+
+import static com.viktorsuetnov.caloriecounting.security.SecurityConstants.TOKEN_PREFIX;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    public static final Logger LOG = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private UserService userService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = getJwtFromRequest(request);
+            if (StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
+                Long userId = jwtTokenProvider.getUserIdFromToken(jwt);
+                User user = userService.getUserById(userId);
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userService, null, Collections.emptyList());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        } catch (Exception exception) {
+            LOG.error("Could not set user authentication");
+        }
+        filterChain.doFilter(request, response);
+    }
 
+    private String getJwtFromRequest(HttpServletRequest request) {
+        String token = request.getHeader(SecurityConstants.HEADER_STRING);
+        if (StringUtils.hasText(token) && token.startsWith(TOKEN_PREFIX)) {
+            return token.split(" ")[1];
+        }
+        return null;
     }
 }
