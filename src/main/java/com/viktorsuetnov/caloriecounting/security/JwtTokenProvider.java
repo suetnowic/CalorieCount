778eac73de80e01af5f1ff2f2d63482bf5d8aa61
Viktor <suetnovvic@yandex.com>
2022-05-11 16:43:17 +0300

    добавлен метод генерации jwt токена

--- src/main/java/com/viktorsuetnov/caloriecounting/security/JwtTokenProvider.java
+++ src/main/java/com/viktorsuetnov/caloriecounting/security/JwtTokenProvider.java
@@ -1,8 +1,41 @@
 package com.viktorsuetnov.caloriecounting.security;
 
+import com.viktorsuetnov.caloriecounting.model.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.viktorsuetnov.caloriecounting.security.SecurityConstants.*;
 
 @Component
 public class JwtTokenProvider {
+
+    public static final Logger LOG = LoggerFactory.getLogger(JwtTokenProvider.class);
+
+    public String generateToken(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        Date now = new Date(System.currentTimeMillis());
+        Date expDate = new Date(now.getTime() + JWT_EXPIRATION_TIME);
+
+        String userId = Long.toString(user.getId());
+
+        Map<String, Object> claimsMap = new HashMap<>();
+        claimsMap.put("id", userId);
+        claimsMap.put("username", user.getEmail());
+        return Jwts.builder()
+                .setSubject(userId)
+                .addClaims(claimsMap)
+                .setIssuedAt(now)
+                .setExpiration(expDate)
+                .signWith(SignatureAlgorithm.HS512, JWT_SECRET)
+                .compact();
+    }
 
 }
