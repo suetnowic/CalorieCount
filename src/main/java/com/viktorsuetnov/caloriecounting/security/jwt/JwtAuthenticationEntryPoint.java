3ae58bc0c72f02e9874b431c46ac9d54f9d9348b
Viktor <suetnovvic@yandex.com>
2022-05-12 11:17:14 +0300

    реализован метод commence

--- src/main/java/com/viktorsuetnov/caloriecounting/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/viktorsuetnov/caloriecounting/security/jwt/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,8 @@
 package com.viktorsuetnov.caloriecounting.security.jwt;
 
+import com.google.gson.Gson;
+import com.viktorsuetnov.caloriecounting.payload.response.InvalidLoginResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -9,11 +12,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static com.viktorsuetnov.caloriecounting.security.SecurityConstants.CONTENT_TYPE;
+
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-
+        InvalidLoginResponse invalidLoginResponse = new InvalidLoginResponse();
+        String jsonLoginResponse = new Gson().toJson(invalidLoginResponse);
+        response.setContentType(CONTENT_TYPE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().println(jsonLoginResponse);
     }
 }
