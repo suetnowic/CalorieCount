0d8970b6b1ac3f83ce665cdfb8f8714909208af0
Viktor <suetnovvic@yandex.com>
2022-05-13 19:38:25 +0300

    добавлен UserNotFoundException

---
+++ src/main/java/com/viktorsuetnov/caloriecounting/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.viktorsuetnov.caloriecounting.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
