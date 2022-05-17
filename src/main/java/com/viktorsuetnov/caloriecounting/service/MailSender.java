3fcfaf4450eb1d5fd7e548091a118c7b14c8700c
Viktor <suetnovvic@yandex.com>
2022-05-17 13:31:46 +0300

    добавлено логирование

--- src/main/java/com/viktorsuetnov/caloriecounting/service/MailSender.java
+++ src/main/java/com/viktorsuetnov/caloriecounting/service/MailSender.java
@@ -1,13 +1,19 @@
 package com.viktorsuetnov.caloriecounting.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+
 @Service
 public class MailSender {
+
+    public static final Logger LOG = LoggerFactory.getLogger(MailSender.class);
 
     private final JavaMailSender mailSender;
 
@@ -20,12 +26,18 @@ public class MailSender {
     }
 
     public void send(String emailTo, String subject, String message) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom(username);
-        mailMessage.setTo(emailTo);
-        mailMessage.setSubject(subject);
-        mailMessage.setText(message);
+        try {
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(username);
+            mailMessage.setTo(emailTo);
+            mailMessage.setSubject(subject);
+            mailMessage.setText(message);
 
-        mailSender.send(mailMessage);
+            mailSender.send(mailMessage);
+        } catch (Exception e) {
+            LOG.error("Failed to send email", e.getMessage());
+            throw new IllegalStateException("Failed to send email");
+        }
+
     }
 }
