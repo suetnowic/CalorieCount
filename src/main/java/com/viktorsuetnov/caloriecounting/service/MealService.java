f44432ea6a1c604d9aba1f87e1b4b4a62ef58722
Viktor <suetnovvic@yandex.com>
2022-05-18 12:24:25 +0300

    изменил параметры методов и возвращаемый тип в интерфейсе MealService

--- src/main/java/com/viktorsuetnov/caloriecounting/service/MealService.java
+++ src/main/java/com/viktorsuetnov/caloriecounting/service/MealService.java
@@ -1,10 +1,12 @@
 package com.viktorsuetnov.caloriecounting.service;
 
+import com.viktorsuetnov.caloriecounting.dto.MealDTO;
 import com.viktorsuetnov.caloriecounting.model.Meal;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Service
@@ -12,16 +14,17 @@ public interface MealService {
 
     Meal createMeal(Meal meal, Principal principal);
 
-    Meal getMeal(long mealId, long userId);
+    Meal getMeal(long mealId, Principal principal);
 
-    List<Meal> getAll(long userId);
+    List<Meal> getAll(Principal principal);
 
-    void updateMeal(Meal meal, Principal principal);
+    Meal updateMeal(Meal meal, Meal mealIn);
 
-    void delete(long mealId);
+    boolean delete(Long id, Principal principal);
 
     List<Meal> getBetweenDates(LocalDate startDate, LocalDate endDate, long userId);
 
-
+    List<MealDTO> getBetweenDatesAndTimes(LocalDate startDate, LocalTime startTime,
+                                       LocalDate endDate, LocalTime endTime, Long userId);
 
 }
