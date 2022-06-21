89b50226a8ef4a87061323515ae12cb726bed10c
Viktor <suetnovvic@yandex.com>
2022-06-21 21:31:55 +0300

    добавлен тест createUser()

--- src/test/java/com/viktorsuetnov/caloriecounting/service/UserServiceTest.java
+++ src/test/java/com/viktorsuetnov/caloriecounting/service/UserServiceTest.java
@@ -1,29 +1,55 @@
 package com.viktorsuetnov.caloriecounting.service;
 
 import com.viktorsuetnov.caloriecounting.exception.UserNotFoundException;
+import com.viktorsuetnov.caloriecounting.model.Role;
 import com.viktorsuetnov.caloriecounting.model.User;
 import com.viktorsuetnov.caloriecounting.repository.UserRepository;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import static com.viktorsuetnov.caloriecounting.TestData.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static com.viktorsuetnov.caloriecounting.TestData.USER1;
-import static com.viktorsuetnov.caloriecounting.TestData.USERS;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
     @Mock
     private UserRepository userRepository;
     @InjectMocks
     private UserServiceImpl userService;
+    @Spy
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Mock
+    private MailSender mailSender;
+
+    @BeforeEach
+    public void init() {
+        bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    }
+
+    @Test
+    public void createUser() {
+        Mockito.doNothing().when(mailSender).send(ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyString());
+        User user = userService.createUser(SIGNUP_REQUEST);
+        Assertions.assertNotNull(user.getActivationCode());
+        Assertions.assertTrue(CoreMatchers.is(user.getRoles()).matches(Collections.singleton(Role.ROLE_USER)));
+        Mockito.verify(userRepository, Mockito.times(1)).save(user);
+    }
 
     @Test
     public void getAllUsers() {
