a860e6b67628c7f74f448f3ab14711e219bf40ee
Viktor <suetnovvic@yandex.com>
2022-06-22 16:33:51 +0300

    добавлен тест метода получения списка пользователей

--- src/test/java/com/viktorsuetnov/caloriecounting/controller/UserControllerTest.java
+++ src/test/java/com/viktorsuetnov/caloriecounting/controller/UserControllerTest.java
@@ -1,5 +1,32 @@
 package com.viktorsuetnov.caloriecounting.controller;
 
+import com.viktorsuetnov.caloriecounting.dto.UserDTO;
+import com.viktorsuetnov.caloriecounting.service.UserServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static com.viktorsuetnov.caloriecounting.TestData.USERS;
+
+@SpringBootTest
 class UserControllerTest {
 
+    @InjectMocks
+    private UserController userController;
+
+    @Mock
+    private UserServiceImpl userService;
+
+    @Test
+    public void getAllUsersTest() {
+        Mockito.when(userService.getAllUsers()).thenReturn(USERS);
+        ResponseEntity<List<UserDTO>> allUsers = userController.getAllUsers();
+        Assertions.assertNotNull(allUsers);
+    }
 }
